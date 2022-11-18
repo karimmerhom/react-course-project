77a463d05deeb2f4fbe38033f26705b971944943
karimmerhom <karimebrahim328@gmail.com>
2022-11-18 23:30:46 +0200

    tutorial 4 react router 6

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-
 import './App.css';
+import {Routes , Route} from 'react-router-dom';
+import Header from './components/header/header.component'
 import HomePage from './pages/homePage/HomePage';
 
 function App() {
   return (
-    <div>
-      <HomePage/>
-    </div>
+    <Routes>
+      <Route path='/' element={<Header/>}>
+        <Route index element={<HomePage/>}>
+        </Route>
+      </Route>
+    </Routes>
   );
 }
 
---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,24 @@
+import React, { Fragment } from 'react';
+import { Outlet , Link} from 'react-router-dom';
+import {ReactComponent as CrownLogo} from '../../assets/crown.svg';
+import './header.styles.scss';
+
+const Header = () => (
+
+    <Fragment>
+        <div className='navigation'>
+            <Link className='nav-link' to='/'>
+                <CrownLogo className='logo-container'/>
+            </Link>
+            <div className='nav-links-container'>
+                <Link className='nav-link' to='/shop'>
+                    SHOP
+                </Link>
+            </div>
+        </div>
+        <Outlet/>
+    </Fragment>
+
+);
+
+export default Header;

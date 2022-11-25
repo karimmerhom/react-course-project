a17539d93b4e411e4a5412bead23654f5ddf273c
karimmerhom <karimebrahim328@gmail.com>
2022-11-25 13:56:42 +0200

    signup form

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,5 +1,8 @@
 import React , {useState} from 'react';
 import {creatAuthUserWithEmailAndPassword, createUserDocumentFromAuth} from '../../utils/firebase/firebase.utils'
+import FormInput from '../form-input/form-input.component'
+import Button from '../button/button.component'
+import './sign-up-form.styles.scss'
 
 const defaultFormFields = {
     displayName: '',
@@ -49,18 +52,15 @@ const SignUpForm = () => {
     }
 
     return (
-        <div>
-            <h1>Sign Up</h1>
+        <div className="sign-up-container">
+            <h2>Don't have an account ?</h2>
+            <span>Sign Up with Email and Password</span>
             <form onSubmit={handleSubmit}>
-                <label>Display Name</label>
-                <input type="text" required onChange={handleChange} name="displayName" value={displayName} minLength={5} maxLength={20}/>
-                <label>Email</label>
-                <input type="email" required onChange={handleChange} name="email" value={email} minLength={5} maxLength={40}/>
-                <label>Password</label>
-                <input type="password" required onChange={handleChange} name="password" value={password} minLength={6} maxLength={20}/>
-                <label>Confirm Password</label>
-                <input type="password" required onChange={handleChange} name="confirmPassword" value={confirmPassword} minLength={6} maxLength={20}/>
-                <button type="submit">"SignUp"</button>
+                <FormInput label="Display Name" type="text" required onChange={handleChange} name="displayName" value={displayName} minLength={5} maxLength={20}/>
+                <FormInput label="Email" type="email" required onChange={handleChange} name="email" value={email} minLength={5} maxLength={40}/>
+                <FormInput label = "Password" type="password" required onChange={handleChange} name="password" value={password} minLength={6} maxLength={20}/>
+                <FormInput label="Confirm password" type="password" required onChange={handleChange} name="confirmPassword" value={confirmPassword} minLength={6} maxLength={20}/>
+                <Button  type="submit">SignUp</Button>
             </form>
         </div>
 )};
